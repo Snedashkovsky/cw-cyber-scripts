dc50928202ab4c5cd7de3be96b8dbe55c68d708a
Snedashkovsky <sn@cyberdrop.ai>
2023-05-06 12:25:35 +0800

    - update config

--- config.py
+++ config.py
@@ -18,10 +18,7 @@ GRAPHQL_URL = 'https://index.bostrom.cybernode.ai/v1/graphql'
 NODE_RPC_URL = 'https://rpc.bostrom.cybernode.ai:443'
 NODE_LCD_URL = 'https://lcd.bostrom.cybernode.ai/'
 CHAIN_ID = 'bostrom'
-LCD_CLIENT = LCDClient(
-    url="https://lcd.bostrom.cybernode.ai/",
-    chain_id="bostrom",
-)
+LCD_CLIENT = LCDClient(url=NODE_LCD_URL, chain_id=CHAIN_ID)
 BASE_COIN_DENOM = 'boot'
 
 CYBERLINK_CREATION_QUERY = './src/create_cyberlink.sh'
